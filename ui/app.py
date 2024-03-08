39db86067304d0d3221ed715c800c55508b98017
Shohei Omoto <omotoshohei@gmail.com>
2024-03-08 13:24:53 +0900

    v5

--- ui/app.py
+++ ui/app.py
@@ -118,6 +118,9 @@ df_wordcloud = pd.read_csv('https://storage.googleapis.com/yelp_review_nlp/df_wo
 df_example = pd.read_csv('https://storage.googleapis.com/yelp_review_nlp/df_example_top_10_b.csv')
 df_lda = pd.read_csv('https://storage.googleapis.com/yelp_review_nlp/df_coefficients_lda.csv')
 df_get_topics = pd.read_csv('https://storage.googleapis.com/yelp_review_nlp/df_get_topics2.csv')
+df_topic_allocation = pd.read_csv('https://storage.googleapis.com/yelp_review_nlp/df_topic_allocation_with_labels.csv')
+
+
 
 # Display the result
 
@@ -190,8 +193,8 @@ if st.session_state["get_result"]:
             value=avg_stars10m_radius,
             domain={'x': [0, 1], 'y': [0, 1]},
             title={
-                'text': "Average Review Score of Other Restaurants Within 10 Miles",
-                'font': {'size': 13}  # Adjust the size as needed
+                'text': "Other Restaurants Within 10 Miles",
+                'font': {'size': 18}  # Adjust the size as needed
             },
             gauge={'axis': {'range': [None, 5]}, 'bar': {'color': "#cdd2f8"}},
             number={'font': {'color': "rgb(34, 34, 34)"}}  # Set number font color to black and adjust size as needed
@@ -247,15 +250,6 @@ if st.session_state["get_result"]:
         ax.set_ylabel('')  # Removes the y-axis label
         st.pyplot(fig)
 
-
-
-
-
-
-
-
-
-
      ########## Display - Examples ##########
     st.text(" ")  # add space
     st.text(" ")  # add space
@@ -282,119 +276,127 @@ if st.session_state["get_result"]:
     st.text(" ")  # add space
 
 
-
-
-    # col1, col2, col3 = st.columns(3)
-
-    # with col1:
-    #     st.button(
-    #         'Get Topics',
-    #         on_click=lambda: st.session_state.update({"get_topics": True})
-    #     ) 
-    #     if st.session_state["get_topics"]:
-    #         st.markdown(f"""
-    #                 <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
-    #                     <h2 style="color: #333;text-align:center;">Get Topics</h2>
-    #                     <p>test</p>
-    #                 </div>
-    #                 """, unsafe_allow_html=True)
-
-    # with col2:
-    #     st.button(
-    #         'Run Regression',
-    #         on_click=lambda: st.session_state.update({"run_regression": True})
-
-    #     ) 
-    #     if st.session_state["run_regression"]:
-    #         st.markdown(f"""
-    #                 <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
-    #                     <h2 style="color: #333;text-align:center;">Run Regression</h2>
-    #                     <p>test</p>
-    #                 </div>
-    #                 """, unsafe_allow_html=True)
-
-    # with col3:
-    #     st.button(
-    #         'Get Suggestions',
-    #         on_click=lambda: st.session_state.update({"get_suggestions": True})
-
-    #     ) 
-    #     if st.session_state["get_suggestions"]:
-    #         st.markdown(f"""
-    #                 <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
-    #                     <h2 style="color: #333;text-align:center;">Suggestion for Improvement</h2>
-    #                     <p>{df_business_filtered['ai_suggestion'].iloc[0]}</p>
-    #                 </div>
-    #                 """, unsafe_allow_html=True)
-
-    ########## Get Topics ##########
+    ########## TF-IDF Explanation ##########
 
     with st.expander("Show TF-IDF explanation"):
         st.write("Review 1: ‘Just serves the best pancakes in town. The service is excellent and the atmosphere is cozy.’ \n\n"
          "Review 2: ‘Just wonderful! The burgers were delicious and the staff was friendly.’ \n\n"
          "Review 3: ‘Just mediocre food and the service was slow.’ \n\n"
          "In ALL reviews, the word ‘just’ appears, so the weight TF-IDF assigns is zero. Conversely, ‘best pancakes’, "
-         "‘burgers delicious’, and ‘service slow’ appear in one review, so their weight will be high. You can use colors "
-         "to differentiate the irrelevant word ‘just’ from the important ones. Just remember, that the word just doesn't add "
-         "any value to the reviews, it's just how people talk.")
+         "‘burgers delicious’, and ‘service slow’ appear in one review, so their weight will be high. "
+         "Just remember, that the word just doesn't add "
+         "any value to the reviews; it's just how people talk.")
         st.image("ui/img/tf-idf.png")
     st.text(" ")  # add space
     st.text(" ")  # add space
     st.text(" ")  # add space
     st.text(" ")  # add space
+    ########## Advanced Modeling ##########
     st.header("Advanced Modeling")
+    ########## Get Topic ##########
+    topic_weights = df_topic_allocation.groupby("topic_label", as_index=False).sum()
+    topic_weights["score"] = topic_weights["score"] / df_topic_allocation["score"].sum()
+    topic_weights = topic_weights.drop(columns="topic")
+    topic_weights = topic_weights.sort_values(by="score", ascending=False)
     st.button(
         'Get Topics',
         on_click=lambda: st.session_state.update({"get_topics": True})
     ) 
     if st.session_state["get_topics"]:
-        # Convert DataFrame to HTML and use style to hide index and header
-        df_html = df_get_topics.to_html(index=False, border=0)
-
-        # Display the DataFrame within the markdown container, ensuring HTML safety
-        st.markdown(f"""
-                    <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
-                        <h2 style="color: #333;text-align:center;">Phrases Allocated Topic</h2>
-                        {df_html}
-                    </div>
-                    """, unsafe_allow_html=True)
-        
-
+        st.text(" ")  # add space
+        st.text(" ")  # add space
+        st.subheader("Phrases Allocated Topic")
+        plt.figure(figsize=(10, 8)) # You can adjust these numbers as per your need
+        ax = sns.barplot(
+            y="topic_label", 
+            x="score", 
+            data=topic_weights, 
+            palette="Greens_d"
+        )
+        ax.tick_params(axis='y', labelsize=10)  # Adjust the size as per your need
+        ax.tick_params(axis='x', labelsize=10)  # Adjust the size as per your need
+        plt.xlabel("Topic Prevalence in Reviews", fontsize=12) # Adjust the font size as per your need
+        plt.ylabel("Topic", fontsize=12)  # Adjust the font size as per your need
+        plt.tight_layout()
+        st.pyplot(plt)
+        st.text(" ")  # add space
+        st.text(" ")  # add space
     with st.expander("Show Topic Modelling (LDA) explanation"):
-        st.write("Latent Dirichlet Allocation (LDA) is an advanced technique used to categorize words into topics.. Initially, words present in text are randomly assigned to topics, and through iterative adjustments, LDA seeks to predict the composition of the original document accurately. This process is grounded in a methodical approach where, over numerous iterations, the model identifies the allocation of words to topics that best reflects the observed text. The effectiveness of LDA stems from its ability to discern the underlying themes within texts, such as ‘service quality’ or ‘atmosphere’, from seemingly unstructured data. By automatically discovering these themes, LDA provides actionable insights into large datasets.")
+        st.text(" ")  # add space
+        st.write("Latent Dirichlet Allocation (LDA) is an advanced technique used to categorize words into topics." 
+                 "Initially, words present in text are randomly assigned to topics, and through iterative adjustments, LDA seeks to predict the composition of the original document accurately."
+                 " This process is grounded in a methodical approach where, over numerous iterations, the model identifies the allocation of words to topics that best reflects the observed text."
+                 " The effectiveness of LDA stems from its ability to discern the underlying themes within texts, such as ‘service quality’ or ‘atmosphere’, from seemingly unstructured data."
+                 " By automatically discovering these themes, LDA provides actionable insights into large datasets.")
+        st.text(" ")  # add space
         st.image("ui/img/lda1.png")
+        st.text(" ")  # add space
         st.image("ui/img/lda2.png")
-
+        st.text(" ")  # add space
 
     ########## Run Regression ##########
-    df_lda_filtered_top5 = df_lda_filtered[df_lda_filtered['Coefficient Type'] == 'Top 5 Positive']
-    df_lda_filtered_bottom5 = df_lda_filtered[df_lda_filtered['Coefficient Type'] == 'Bottom 5 Negative']
-    # Get a list of features for top 5 positive
-    top_5_positive_features = df_lda_filtered_top5['Feature'].tolist()
-    # Join into a single string with a separator like a comma or line break
-    top_5_positive_features_str = ', '.join(top_5_positive_features)
+    # df_lda_filtered_top5 = df_lda_filtered[df_lda_filtered['Coefficient Type'] == 'Top 5 Positive']
+    # df_lda_filtered_bottom5 = df_lda_filtered[df_lda_filtered['Coefficient Type'] == 'Bottom 5 Negative']
+    # # Get a list of features for top 5 positive
+    # top_5_positive_features = df_lda_filtered_top5['Feature'].tolist()
+    # # Join into a single string with a separator like a comma or line break
+    # top_5_positive_features_str = ', '.join(top_5_positive_features)
 
-    # Get a list of features for bottom 5 negative
-    bottom_5_negative_features = df_lda_filtered_bottom5['Feature'].tolist()
-    # Join into a single string with a separator like a comma or line break
-    bottom_5_negative_features_str = ', '.join(bottom_5_negative_features)
+    # # Get a list of features for bottom 5 negative
+    # bottom_5_negative_features = df_lda_filtered_bottom5['Feature'].tolist()
+    # # Join into a single string with a separator like a comma or line break
+    # bottom_5_negative_features_str = ', '.join(bottom_5_negative_features)
+    ##################################################################################################################
+    ##################################################################################################################
+    ##################################################################################################################
+    ##################################################################################################################
+    ##################################################################################################################
+    ##################################################################################################################
+    ##################################################################################################################
     st.button(
         'Run Regression',
         on_click=lambda: st.session_state.update({"run_regression": True})
 
     ) 
     if st.session_state["run_regression"]:
-        st.markdown(f"""
-                <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
-                    <h2 style="color: #333;text-align:center;">Most Frequent Topics Mentioned</h2>
-                    <p style="text-align:center;"><b>Top 5 Positive:</b> {top_5_positive_features_str}</p>
-                    <p style="text-align:center;"><b>Bottom 5 Negative:</b> {bottom_5_negative_features_str}</p>
-                </div>
-                """, unsafe_allow_html=True)
-        
+        # st.markdown(f"""
+        #         <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
+        #             <h2 style="color: #333;text-align:center;">Most Frequent Topics Mentioned</h2>
+        #             <p style="text-align:center;"><b>Top 5 Positive:</b> {top_5_positive_features_str}</p>
+        #             <p style="text-align:center;"><b>Bottom 5 Negative:</b> {bottom_5_negative_features_str}</p>
+        #         </div>
+        #         """, unsafe_allow_html=True)
+        st.subheader("Topics Driving Positive/Negative Reviews")
+
+        col6, col7 = st.columns(2)
+        with col6:
+            st.subheader("Top 5 Positive")
+            df_lda_positive = df_lda_filtered.nlargest(5, 'Coefficient')
+            fig, ax = plt.subplots()
+            sns.barplot(x='Coefficient', y='Feature', data=df_lda_positive, ax=ax, palette="Blues_d")
+            plt.xlabel('Positive Words')
+            ax.set_ylabel('')  # Removes the y-axis label
+            st.pyplot(fig)
+
+        with col7:
+            st.subheader("Top 5 Negative")
+            df_lda_negative = df_lda_filtered.nsmallest(5, 'Coefficient')
+            fig, ax = plt.subplots()
+            sns.barplot(x='Coefficient', y='Feature', data=df_lda_negative, ax=ax, palette="Reds_d")
+            plt.xlabel('Negative')
+            ax.set_ylabel('')  # Removes the y-axis label
+            st.pyplot(fig)
+
+
 
     with st.expander("Show Linear Regression Explanation"):
-        st.write("Imagine you're listening to people talk about Mother's Restaurant. Some say the ‘chicken wings are amazing’, while others complain aout ‘rude servicbe’. All this talk goes on for a long time on places like Yelp or social media. Now, if we want to figure out how much these good or bad comments affect the restaurant's overall rating, we use something called regression. Regression is like a magic math trick. It helps us see how each thing people mention, like ‘great chicken wings’ or ‘rude service’, affects the restaurant's score giving us impact coefficients. The cool part is these coefficients tell us exactly what needs fixing to get better ratings.")
+        st.text(" ")  # add space
+        st.write("Imagine you're listening to people talk about Mother's Restaurant."
+                 " Some say the ‘chicken wings are amazing’, while others complain about ‘rude service’. "
+                 "All this talk goes on for a long time on places like Yelp or social media. Now, if we want to figure out how much these good or bad comments affect the restaurant's overall rating, we use something called regression."
+                 " Regression is like a magic math trick. It helps us see how each thing people mention, like ‘great chicken wings’ or ‘rude service’, affects the restaurant's score giving us impact coefficients."
+                 " The cool part is these coefficients tell us exactly what needs fixing to get better ratings.")
+        st.text(" ")  # add space
         st.image("ui/img/linear_regression.png")
     ########## Get Suggestion ##########
     # get_suggestions = st.button('Get Suggestions')
