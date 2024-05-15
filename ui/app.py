66c23350165c75b872c1b191128fe71b48a11eb7
Shohei Omoto <omotoshohei@gmail.com>
2024-05-15 12:30:34 +0900

    update app.py

--- ui/app.py
+++ ui/app.py
@@ -4,13 +4,15 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 import streamlit as st
 # import os
-from wordcloud import WordCloud
+
 # import matplotlib.pyplot as plt
-import openai
-import ast
+# import openai
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
+
+DEPLOYED = True
+img_folder_path = "ui/img"
 
 st.set_page_config(layout="wide")
 
@@ -79,7 +81,7 @@ table.dataframe {
 }
 /* ---- Change the font color for expander ---- */
 .st-emotion-cache-1h9usn1 {
-    background-color: #f7f7f7;
+    background-color: #f2f2f2;
 }
 </style>
 """,
@@ -100,18 +102,21 @@ if "get_suggestions" not in st.session_state:
 ############# Display - Title / User Input ###################
 left_co,left2_co, cent_co,last2_co,last_co = st.columns([2,2,3,2,2])
 with cent_co:
-    st.image('ui/img/logo_nlpalate_200.png', use_column_width=True, width=180)
+    st.image(f'{img_folder_path}/logo_nlpalate_200.png', use_column_width=True, width=180)
 
 with st.form(key='user_input_form'):
     col1,col2,col3 = st.columns([2,3,2])
 
     with col2:
-        name = st.text_input("Restaurant Name", max_chars=50)
+        name = st.text_input("Restaurant Name.", max_chars=50)
         st.text(" ")
         st.form_submit_button(
             'Get Results',
             on_click=lambda: st.session_state.update({"get_result": True})
         )
+        st.text("Due to storage limitations, application is currently")
+        st.text("limited to restaurants in New Orleans.")
+        st.text("For a demo, try 'Luke' or 'Mother's Restaurant'.")
 
 # Read CSV
 df_review = pd.read_csv('data/df_review_top10.csv')
@@ -124,8 +129,9 @@ df_lda = pd.read_csv('data/df_coefficients_lda.csv')
 df_get_topics = pd.read_csv('data/df_get_topics2.csv')
 df_topic_allocation = pd.read_csv('data/df_topic_allocation_with_labels.csv')
 
-# Display the result
 
+
+# Display the result
 df_get_topics = df_get_topics[['topic_label', 'phrase', 'score']]
 df_get_topics = df_get_topics.rename(columns={'topic_label': 'Topic', 'phrase': 'Phrase', 'score': 'Score'})
 
@@ -169,7 +175,9 @@ if st.session_state["get_result"]:
             mode="gauge+number",
             value=average_reviews,
             domain={'x': [0, 1], 'y': [0, 1]},
-            title={'text': "Average Review Scores"},
+            title={'text': f"Average Review Score for {name}",
+                    'font': {'size': 18} # Adjust the size as needed},
+            },
             gauge={'axis': {'range': [None, 5]}, 'bar': {'color': "#7d8beb"}},
             number={'font': {'color': "rgb(34, 34, 34)"}}  # Set number font color to black and adjust size as needed
         ))
@@ -205,41 +213,45 @@ if st.session_state["get_result"]:
         st.plotly_chart(fig, use_container_width=True)
 
     ########## Display - Word Clouds ##########
-    st.header("Frequently Mentioned Keywords")
-    col10, col11 = st.columns(2)
+    if DEPLOYED:
+        from wordcloud import WordCloud
+        import ast
+        st.header("Frequently Mentioned Keywords")
+        col10, col11 = st.columns(2)
 
-    with col10:
-        st.subheader("Our Restaurant")
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        wc_own_dict = df_wordcloud_filtered["own_wc_dict"].iloc[0]
-        wc_own_dict = ast.literal_eval(wc_own_dict)
-        wc_own = WordCloud(width=800,
-                      height=400,
-                      background_color='white').fit_words(wc_own_dict)
-        st.image(wc_own.to_array())
+        with col10:
+            st.subheader("Our Restaurant")
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            wc_own_dict = df_wordcloud_filtered["own_wc_dict"].iloc[0]
+            wc_own_dict = ast.literal_eval(wc_own_dict)
+            wc_own = WordCloud(width=800,
+                        height=400,
+                        background_color='white').fit_words(wc_own_dict)
+            st.image(wc_own.to_array())
 
-    with col11:
-        st.subheader("Our Competitors")
-        wc_other_dict = df_wordcloud_filtered["other_wc_dict"].iloc[0]
-        wc_other_dict = ast.literal_eval(wc_other_dict)
-        wc_other = WordCloud(width=800,
-                      height=400,
-                      colormap = 'BuPu_r',
-                      background_color='white').fit_words(wc_other_dict)
-        st.image(wc_other.to_array())
+        with col11:
+            st.subheader("Our Competitors")
+            wc_other_dict = df_wordcloud_filtered["other_wc_dict"].iloc[0]
+            wc_other_dict = ast.literal_eval(wc_other_dict)
+            wc_other = WordCloud(width=800,
+                        height=400,
+                        colormap = 'BuPu_r',
+                        background_color='white').fit_words(wc_other_dict)
+            st.image(wc_other.to_array())
 
-    st.text(" ")  # add space
-    st.text(" ")  # add space
+        st.text(" ")  # add space
+        st.text(" ")  # add space
+
     st.text(" ")  # add space
     st.text(" ")  # add space
     ########## Display - Top 5 Compliments / Complaints ##########
     col6, col7 = st.columns(2)
     with col6:
-        st.header("Top 5 Praises")
+        st.header("Top 5 Compliments")
         top_praises = df_example_filtered.nlargest(5, 'praise_coeff')
         fig, ax = plt.subplots()
         sns.barplot(x='praise_coeff', y='praise_words', data=top_praises, ax=ax, palette="Blues_d")
-        plt.xlabel('Praise Score')
+        plt.xlabel('Compliment Score')
         ax.set_ylabel('')  # Removes the y-axis label
         st.pyplot(fig)
 
@@ -257,7 +269,7 @@ if st.session_state["get_result"]:
     st.text(" ")  # add space
     col8, col9 = st.columns(2)
     with col8:
-        st.header("Praise Examples")
+        st.header("Compliment Examples")
         df_example_filtered_praise_order = df_example_filtered.nlargest(5, 'praise_coeff')
 
         for index, row in df_example_filtered_praise_order.iterrows():
@@ -281,20 +293,18 @@ if st.session_state["get_result"]:
     ########## TF-IDF Explanation ##########
 
     with st.expander("Show TF-IDF explanation"):
-        st.write("Review 1: ‘Just serves the best pancakes in town. The service is excellent and the atmosphere is cozy.’ \n\n"
-         "Review 2: ‘Just wonderful! The burgers were delicious and the staff was friendly.’ \n\n"
-         "Review 3: ‘Just mediocre food and the service was slow.’ \n\n"
-         "In ALL reviews, the word ‘just’ appears, so the weight TF-IDF assigns is zero. Conversely, ‘best pancakes’, "
-         "‘burgers delicious’, and ‘service slow’ appear in one review, so their weight will be high. "
-         "Just remember, that the word just doesn't add "
-         "any value to the reviews; it's just how people talk.")
-        st.image("ui/img/tf-idf.png")
+        st.write("We want to find the most commonly occurring phrases in positive reviews, and in negative reviews. "
+                " However when you look at the english language, the phrases or words that most commonly appear are 'and', 'We went to'. "
+                " These Phrases provide no actual insight into the reasons behind the positive and negative reviews."
+                " So instead of just taking a count of the number of times these phrases appear, we can calculate a 'score' that penalizes phrases that appear across ALL reviews, "
+                " which then allows us to uncover which phrases appear more common for ONLY that restaurant.")
+        st.image(f"{img_folder_path}/tf-idf.png")
     st.text(" ")  # add space
     st.text(" ")  # add space
     st.text(" ")  # add space
     st.text(" ")  # add space
-    ########## Advanced Modeling ##########
-    st.header("Advanced Modeling")
+    ########## Advanced Modelling ##########
+    st.header("Advanced Modelling")
     ########## Get Topic ##########
     topic_weights = df_topic_allocation.groupby("topic_label", as_index=False).sum()
     topic_weights["score"] = topic_weights["score"] / df_topic_allocation["score"].sum()
@@ -323,17 +333,20 @@ if st.session_state["get_result"]:
         st.pyplot(plt)
         st.text(" ")  # add space
         st.text(" ")  # add space
+
+    st.text(" ")
+    st.text(""" For more in depth insights, we can use machine learning to extract just 30 topics from all 500,000 reviews in our dataset. This way, our insights are
+more generallly applicable""")
+
     with st.expander("Show Topic Modelling (LDA) explanation"):
         st.text(" ")  # add space
         st.write("Latent Dirichlet Allocation (LDA) is an advanced technique used to categorize words into topics."
-                 "Initially, words present in text are randomly assigned to topics, and through iterative adjustments, LDA seeks to predict the composition of the original document accurately."
+                 " Initially, words present in text are randomly assigned to topics, and through iterative adjustments, LDA seeks to predict the composition of the original document accurately."
                  " This process is grounded in a methodical approach where, over numerous iterations, the model identifies the allocation of words to topics that best reflects the observed text."
                  " The effectiveness of LDA stems from its ability to discern the underlying themes within texts, such as ‘service quality’ or ‘atmosphere’, from seemingly unstructured data."
                  " By automatically discovering these themes, LDA provides actionable insights into large datasets.")
         st.text(" ")  # add space
-        st.image("ui/img/lda1.png")
-        st.text(" ")  # add space
-        st.image("ui/img/lda2.png")
+        st.image(f"{img_folder_path}/lda2.png")
         st.text(" ")  # add space
 
     ########## Run Regression ##########
@@ -356,18 +369,11 @@ if st.session_state["get_result"]:
     ##################################################################################################################
     ##################################################################################################################
     st.button(
-        'Run Regression',
+        'Run Regression on Topics',
         on_click=lambda: st.session_state.update({"run_regression": True})
 
     )
     if st.session_state["run_regression"]:
-        # st.markdown(f"""
-        #         <div style="background-color: rgb(240, 242, 246); border-radius: 10px; padding: 20px; margin: 10px 0;">
-        #             <h2 style="color: #333;text-align:center;">Most Frequent Topics Mentioned</h2>
-        #             <p style="text-align:center;"><b>Top 5 Positive:</b> {top_5_positive_features_str}</p>
-        #             <p style="text-align:center;"><b>Bottom 5 Negative:</b> {bottom_5_negative_features_str}</p>
-        #         </div>
-        #         """, unsafe_allow_html=True)
         st.subheader("Topics Driving Positive/Negative Reviews")
 
         col6, col7 = st.columns(2)
@@ -393,13 +399,13 @@ if st.session_state["get_result"]:
 
     with st.expander("Show Linear Regression Explanation"):
         st.text(" ")  # add space
-        st.write("Imagine you're listening to people talk about Mother's Restaurant."
-                 " Some say the ‘chicken wings are amazing’, while others complain about ‘rude service’. "
-                 "All this talk goes on for a long time on places like Yelp or social media. Now, if we want to figure out how much these good or bad comments affect the restaurant's overall rating, we use something called regression."
-                 " Regression is like a magic math trick. It helps us see how each thing people mention, like ‘great chicken wings’ or ‘rude service’, affects the restaurant's score giving us impact coefficients."
-                 " The cool part is these coefficients tell us exactly what needs fixing to get better ratings.")
+        st.write(" To find which topics are having the most impact on review score, we can use linear regression. "
+                " This will take the appearance of each topic in all the reviews, and generate a model that will try to predict the review score of a new review, based on the presence of each."
+                " Linear regression gives us our best approximation of a simple equation that makes this prediction."
+                " The equation gives us a formula gives a coefficient, indication of how much the appearance of each topic affects review score."
+                " We can take this coefficient as a 'strength score' of each topic on how much they affect the final review score.")
         st.text(" ")  # add space
-        st.image("ui/img/linear_regression.png")
+        st.image(f"{img_folder_path}/linear_regression.png")
     ########## Get Suggestion ##########
     # get_suggestions = st.button('Get Suggestions')
 
@@ -416,6 +422,16 @@ if st.session_state["get_result"]:
                 </div>
                 """, unsafe_allow_html=True)
 
+
+    with st.expander("Querying OpenAI for Suggestions"):
+        st.text(" ")  # add space
+        st.write("""To provide business owners with what improvements can actually
+                 be made to improve their restaurant's reviews, we use a plugin to OpenAI's API.
+                 We send all the above information, as will as a specially tailored prompt to query the engine.
+                 ChatGPT reads the provided data and prompts,
+                 and provides a summary in a more natural, easy to digest format.""")
+        st.text(" ")  # add space
+        st.image(f"{img_folder_path}/openai_logo.png", width = 400)
 
 
 
